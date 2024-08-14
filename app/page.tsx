38675dfe120d3821b73640578c7569767a509de3
Huann Almeida <huannvictor@gmail.com>
2024-08-14 00:14:21 -0300

    iniciando confecção de telas

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
-import Image from "next/image";
+import Image from "next/image"
+import Header from "./_components/header"
+import { Input } from "./_components/ui/input"
+import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
 
 export default function Home() {
   return (
-    <div className="bg-red-200 text-zinc-800 py-2 mt-1">texto</div>
-  );
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Huann!</h2>
+        <p>Terça-feira, 13 de agosto de 2024</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative h-[150] w-full">
+          <Image
+            alt="banner"
+            src="/banner-01.png"
+            className="object-cover"
+            fill
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
