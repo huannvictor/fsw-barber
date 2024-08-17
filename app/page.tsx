baa63315d538f3781fdb6e786ec4bd7f852fa0dc
Huann Almeida <huannvictor@gmail.com>
2024-08-17 19:00:51 -0300

    feat: add Footer

--- app/page.tsx
+++ app/page.tsx
@@ -87,6 +87,17 @@ const Home = async () => {
           ))}
         </div>
       </div>
+
+      {/* FOOTER */}
+      <footer>
+        <Card className="px-5 py-6">
+          <CardContent>
+            <p className="text-sm text-gray-400">
+              © 2024 Copyright <span className="font-bold">FSW Barber</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
