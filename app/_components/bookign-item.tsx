3368089b4a40569778fe2e6a2ba40b3c5b242520
Huann Almeida <huannvictor@gmail.com>
2024-08-17 20:40:01 -0300

    feat: BookingItem component

---
+++ app/_components/bookign-item.tsx
@@ -0,0 +1,40 @@
+import { Avatar, AvatarImage } from "./ui/avatar"
+import { Badge } from "./ui/badge"
+import { Card, CardContent } from "./ui/card"
+
+//TODO: receber agendamento como propriedade
+
+const BookingItem = () => {
+  return (
+    <>
+      <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+        agendamentos
+      </h2>
+      <Card>
+        <CardContent className="flex justify-between p-0">
+          {/* LEFT CONTENT */}
+          <div className="flex flex-col gap-2 py-5 pl-5">
+            <Badge className="w-fit">Confirmado</Badge>
+            <h3 className="font-semibold">Corte de Cabelo</h3>
+
+            <div className="flex items-center gap-2">
+              <Avatar className="h-6 w-6">
+                <AvatarImage src="https://utfs.io/f/45331760-899c-4b4b-910e-e00babb6ed81-16q.png" />
+              </Avatar>
+              <p className="text-sm">Barbearia FSW</p>
+            </div>
+          </div>
+
+          {/* RIGHT CONTENT */}
+          <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+            <p className="text-sm">Agosto</p>
+            <p className="text-2xl">20</p>
+            <p className="text-sm">20:00</p>
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  )
+}
+
+export default BookingItem
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,10 @@ import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
 import { SearchIcon } from "lucide-react"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
 import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/bookign-item"
 
 const Home = async () => {
   // call database
@@ -59,32 +58,8 @@ const Home = async () => {
         </div>
 
         {/* BOOKING */}
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/* LEFT CONTENT */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
 
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/45331760-899c-4b4b-910e-e00babb6ed81-16q.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-
-            {/* RIGHT CONTENT */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">20</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           recomendados
