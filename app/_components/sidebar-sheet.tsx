2215a9ab80ae0491ad117429548e078306b7fceb
Huann Almeida <huannvictor@gmail.com>
2024-08-27 20:00:57 -0300

    feat: ensure logout button is shown only to signed in users

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -86,16 +86,18 @@ const SidebarSheet = () => {
         ))}
       </div>
 
-      <div className="flex flex-col gap-2 py-5">
-        <Button
-          className="justify-start gap-2"
-          variant={"ghost"}
-          onClick={handleLogoutClick}
-        >
-          <LogOutIcon size={18} />
-          Sair da Conta
-        </Button>
-      </div>
+      {data?.user && (
+        <div className="flex flex-col gap-2 py-5">
+          <Button
+            className="justify-start gap-2"
+            variant={"ghost"}
+            onClick={handleLogoutClick}
+          >
+            <LogOutIcon size={18} />
+            Sair da Conta
+          </Button>
+        </div>
+      )}
     </SheetContent>
   )
 }
